cd020ab2b4651496c6ee9758c4c58ad7d7d6def1
Isimsiz <daisimsiz.tr@gmail.com>
2022-06-08 21:57:40 +0300

    oops...

--- Writer/Program.cs
+++ Writer/Program.cs
@@ -11,12 +11,12 @@ namespace Writer
             while(true)
             {
                 Console.WriteLine("Waiting for input...");
-                input = Console.ReadLine().ToCharArray();
+                input = Console.ReadLine().ToCharArray();   //Converting console input into a list of characters
                 Console.Clear();
                 foreach(char i in input)
                 {
-                    Console.Write(i);
-                    Thread.Sleep(125);
+                    Console.Write(i);                       //Writing the characters in order 1 by 1
+                    Thread.Sleep(125);                      //And waiting 125ms between each character
                 }
                 Console.WriteLine("\n");
             }
